4d76cabd39be886f20d5e72552b71fc1e1115fa8
Camilo Isaza <camiloisazag@hotmail.com>
2023-11-27 00:02:22 +0100

    Add challenge functionality

---
+++ src/middleware/auth.interceptor.ts
@@ -0,0 +1,44 @@
+import createDebug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import { HttpError } from '../types/http.error.js';
+import { Auth } from '../services/auth.js';
+import { AuthenticatedRequest } from '../controllers/users.controller.js';
+
+const debug = createDebug('W7E:auth:interceptor');
+
+export class AuthInterceptor {
+  constructor() {
+    debug('Instantiated');
+  }
+
+  authorization(req: Request, res: Response, next: NextFunction) {
+    try {
+      const tokenHeader = req.get('Authorization');
+      if (!tokenHeader?.startsWith('Bearer'))
+        throw new HttpError(401, 'Unauthorized');
+      const token = tokenHeader.split(' ')[1];
+      const tokenPayload = Auth.verifyAndGetPayload(token);
+      req.body.userId = tokenPayload.id;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  authorization2(req: Request, res: Response, next: NextFunction) {
+    try {
+      const tokenHeader = req.get('Authorization');
+      if (!tokenHeader?.startsWith('Bearer'))
+        throw new HttpError(401, 'Unauthorized');
+      const token = tokenHeader.split(' ')[1];
+      const tokenPayload = Auth.verifyAndGetPayload(token);
+
+      // Asignar tokenPayload a req
+      (req as AuthenticatedRequest).tokenPayload = tokenPayload;
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+}
